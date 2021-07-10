978e8af0ea57d4115bcafa3c908f93e4793064b3
wendell <wendell_101@hotmail.com>
2021-07-10 10:48:01 -0300

    Integrando e testando o banco h2

--- Curso-JavaWeb/src/main/java/com/example/couse/entites/User.java
+++ Curso-JavaWeb/src/main/java/com/example/couse/entites/User.java
@@ -3,7 +3,17 @@ package com.example.couse.entites;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class User implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
