e121c489b77d559f0aadebda203805e322a1a846
wendell <wendell_101@hotmail.com>
2021-07-10 11:49:04 -0300

    Criação do Service, component registration

---
+++ Curso-JavaWeb/src/main/java/com/example/couse/services/UserService.java
@@ -0,0 +1,28 @@
+package com.example.couse.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.couse.entites.User;
+import com.example.couse.repositories.UserReposity;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserReposity reposity;
+
+	public List<User> findAll() {
+		return reposity.findAll();
+
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = reposity.findById(id);
+		return obj.get();
+	}
+
+}
