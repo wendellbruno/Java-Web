7f71a24fb8e9f26ae5ef07562c5a6becfdf8d2ba
wendell <wendell_101@hotmail.com>
2021-07-10 09:51:29 -0300

    Primeiro commit do projeto

---
+++ Curso-JavaWeb/src/main/java/com/example/couse/CursoJavaWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.couse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJavaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJavaWebApplication.class, args);
+	}
+
+}
