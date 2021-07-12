c63374b76081b1aef431af3855e5ec88c3e287ef
wendell <wendell_101@hotmail.com>
2021-07-12 10:41:11 -0300

    Criação da classe Order, instant com ISO 8601

--- Curso-JavaWeb/src/main/java/com/example/couse/entites/User.java
+++ Curso-JavaWeb/src/main/java/com/example/couse/entites/User.java
@@ -1,15 +1,20 @@
 package com.example.couse.entites;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 
 @Entity
+@Table(name ="tb_user")
 public class User implements Serializable{
 	
 	@Id
@@ -19,6 +24,10 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	
 	public User() {
 		
@@ -72,12 +81,16 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
 
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
