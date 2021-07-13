db62f0d4594222ce79ccdaa53a028a21e2ea31ea
wendell <wendell_101@hotmail.com>
2021-07-13 10:56:14 -0300

    Criação e associação do pagamento

--- Curso-JavaWeb/src/main/java/com/example/couse/config/TestConfig.java
+++ Curso-JavaWeb/src/main/java/com/example/couse/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.couse.entites.Category;
 import com.example.couse.entites.Order;
 import com.example.couse.entites.OrderItem;
+import com.example.couse.entites.Payment;
 import com.example.couse.entites.Product;
 import com.example.couse.entites.User;
 import com.example.couse.entites.enums.OrderStatus;
@@ -22,22 +23,22 @@ import com.example.couse.repositories.UserReposity;
 
 @Configuration
 @Profile("test")
-public class TestConfig implements CommandLineRunner{
+public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private UserReposity userReposity;
-	
+
 	@Autowired
 	private OrderReposity orderReposity;
-	
+
 	@Autowired
 	private CategoryReposity categoryReposity;
-	
+
 	@Autowired
 	private ProductReposity productReposity;
-	
+
 	@Autowired
 	private OrderItemReposity orderItemReposity;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -68,9 +69,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.WAITING_PAYMENT, u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.DELIVERY, u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.SHIPPED,u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT,u1);
 		
 		userReposity.saveAll(Arrays.asList(u1,u2));
 		orderReposity.saveAll(Arrays.asList(o1,o2,o3));
@@ -83,8 +84,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemReposity.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderReposity.save(o1);
+		
 	}
-	
-	
 
 }
--- Curso-JavaWeb/src/main/java/com/example/couse/entites/Order.java
+++ Curso-JavaWeb/src/main/java/com/example/couse/entites/Order.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.example.couse.entites.enums.OrderStatus;
@@ -34,9 +36,12 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
+
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 
 	public Order() {
 
@@ -83,9 +88,17 @@ public class Order implements Serializable {
 			this.orderStatus = orderStatus.getCode();
 		}
 	}
-	
-	public Set<OrderItem> getItem(){
+
+	public Set<OrderItem> getItem() {
 		return items;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	@Override
